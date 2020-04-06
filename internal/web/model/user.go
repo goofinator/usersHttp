44f92b8df8940bc5435a19cea2df6d1215e924d4
Blinnikov Andrei <goofinator@mail.ru>
2020-04-07 00:56:02 +0300

    feat: json metadata added to model.User

--- internal/web/model/user.go
+++ internal/web/model/user.go
@@ -4,9 +4,9 @@ import "time"
 
 // User structure stores user data
 type User struct {
-	ID        int
-	Name      string
-	Lastname  string
-	Age       int
-	Birthdate time.Time
+	ID        int       `json:"Id"`
+	Name      string    `json:"Name"`
+	Lastname  string    `json:"Lastname"`
+	Age       int       `json:"Age"`
+	Birthdate time.Time `json:"Birthdate"`
 }
