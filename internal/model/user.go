a2d55a13c65f2581063369480c655acdc3de593f
Blinnikov Andrei <goofinator@mail.ru>
2020-04-08 20:10:47 +0300

    feat: fields of model.User are not used at the input, so they may be omitted in json

--- internal/model/user.go
+++ internal/model/user.go
@@ -4,9 +4,9 @@ import "time"
 
 // User structure stores user data
 type User struct {
-	ID        int       `json:"Id"`
+	ID        int       `json:"Id,omitempty"`
 	Name      string    `json:"Name"`
 	Lastname  string    `json:"Lastname"`
-	Age       int       `json:"Age"`
+	Age       int       `json:"Age,omitempty"`
 	Birthdate time.Time `json:"Birthdate"`
 }
