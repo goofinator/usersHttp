56c0093105bcb0c6fc55ca441f6169da1df2a64f
Blinnikov Andrei <goofinator@mail.ru>
2020-04-09 01:13:24 +0300

    test: add mocks for user's repository to test user's service

--- internal/repositories/user.go
+++ internal/repositories/user.go
@@ -1,3 +1,6 @@
+//go:generate mockgen -destination=./mocks/mock_user.go -package=mocks github.com/goofinator/usersHttp/internal/repositories User
+//go:generate goimports -w ./mocks/mock_user.go
+
 package repositories
 
 import (
